038be868da0b8b99c9b099e78aaa78eeab510432
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-04-04 11:23:30 +0700

    api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -10,5 +10,19 @@ module.exports = {
       } catch (error) {
          req.status(500).json({ message: error.message || 'Internal server error'})
       }
+   },
+   detailPage: async(req, res)=>{
+      try {
+         const { id } = req.params 
+         const voucher = await Voucher.findOne({_id: id}).populate('category').populate('nominals').populate('user', '_id name phoneNumber')
+
+         if(!voucher){
+            res.status(404).json({ message: 'Voucher not found' })
+         }
+
+         res.status(200).json({ data: voucher })
+      } catch (error) {
+         req.status(500).json({ message: error.message || 'Internal server error'})
+      }
    }
 }
