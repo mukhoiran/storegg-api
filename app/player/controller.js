0cba963948f33853efbd1520c51e3ffe49761ff5
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-04-04 10:51:45 +0700

    make data more specific

--- app/player/controller.js
+++ app/player/controller.js
@@ -4,7 +4,7 @@ const Voucher = require('../voucher/model')
 module.exports = {
    landingPage: async(req, res)=>{
       try {
-         const voucher = await Voucher.find().select(' _id name status category thumbnail').populate('category')
+         const voucher = await Voucher.find().select(' _id name status category thumbnail ').populate('category')
 
          res.status(200).json({ data: voucher })
       } catch (error) {
