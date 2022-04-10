84147a304795118723da9c77d6a952900b160d4d
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-04-10 11:58:05 +0700

    api transactions history

--- app/player/controller.js
+++ app/player/controller.js
@@ -13,7 +13,7 @@ module.exports = {
 
          res.status(200).json({ data: voucher })
       } catch (error) {
-         req.status(500).json({ message: error.message || 'Internal server error'})
+         res.status(500).json({ message: error.message || 'Internal server error'})
       }
    },
    detailPage: async(req, res)=>{
@@ -27,7 +27,7 @@ module.exports = {
 
          res.status(200).json({ data: voucher })
       } catch (error) {
-         req.status(500).json({ message: error.message || 'Internal server error'})
+         res.status(500).json({ message: error.message || 'Internal server error'})
       }
    },
    category: async(req, res)=>{
@@ -36,7 +36,7 @@ module.exports = {
          res.status(200).json({ data: category })
 
       } catch (error) {
-         req.status(500).json({ message: error.message || 'Internal server error'})         
+         res.status(500).json({ message: error.message || 'Internal server error'})         
       }
    },
    checkout: async(req, res)=>{
@@ -92,7 +92,51 @@ module.exports = {
          res.status(201).json({ data: transaction })
 
       } catch (error) {
-         req.status(500).json({ message: error.message || 'Internal server error'})         
+         res.status(500).json({ message: error.message || 'Internal server error'})         
       }
    },
+   history: async(req, res)=>{
+      try {
+         const { status = '' } = req.query
+
+         let criteria = {}
+
+         if(status.length){
+            criteria = {
+               ...criteria,
+               status: {
+                  $regex : `${status}`,
+                  $options : 'i'
+               }
+            }
+         }
+
+         if(req.player._id){
+            criteria = {
+               ...criteria,
+               player: req.player._id
+            }
+         }
+
+         const history = await Transaction.find(criteria)
+
+         let total = await Transaction.aggregate([
+            {$match: criteria},
+            {
+               $group: {
+                  _id : null,
+                  value: { $sum: "$value"}
+               }
+            }
+         ])
+
+         res.status(200).json({ 
+            data: history, 
+            total: total.length ? total[0].value : 0
+         })
+
+      } catch (error) {
+         res.status(500).json({ message: error.message || 'Internal server error'})                  
+      }
+   }
 }
